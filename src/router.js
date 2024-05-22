f9e818a08fc5de9708e15bcd9914de0eac3c99ea
carl jackson <c.jackson@eagles.oc.edu>
2024-05-21 19:03:11 -0500

    The initial ui design for the menu

--- src/router.js
+++ src/router.js
@@ -24,6 +24,41 @@ const router = createRouter({
       name: "ingredients",
       component: () => import("./views/IngredientList.vue"),
     },
+    {
+      path: "/maintain",
+      name: "maintain",
+      component: () => import("./views/StoryMaintain.vue"),
+    },
+    {
+      path: "/stories",
+      name: "stories",
+      component: () => import("./views/StoriesList.vue"),
+    },
+    {
+      path: "/characters",
+      name: "characters",
+      component: () => import("./views/StoryCharacterRoles.vue"),
+    },
+    {
+      path: "/countries",
+      name: "countries",
+      component: () => import("./views/StoryCountries.vue"),
+    },
+    {
+      path: "/languages",
+      name: "languages",
+      component: () => import("./views/StoryLanguages.vue"),
+    },
+    {
+      path: "/settings",
+      name: "settings",
+      component: () => import("./views/StorySettings.vue"),
+    },
+    {
+      path: "/storypdf",
+      name: "storypdf",
+      component: () => import("./views/StoryPDF.vue"),
+    },
   ],
 });
 
---
+++ src/views/StoryMaintain.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { onMounted } from "vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import UserServices from "../services/UserServices";
+    
+</script>
+
+<style>
+.container {
+  height: 200px;
+  position: relative;
+}
+
+.vertical-center {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+</style>
+
+<template>
+  <v-container>
+    <div class=".justify-content-xxl-center .align-center ">
+        <!-- Ignore the mess for now, fixing spacing later - CJ 5/21/2024 -->
+        <v-row justify="center">
+        <v-col cols="auto">
+        </v-col>
+        </v-row>
+        <v-row justify="center">
+        <v-col cols="auto">
+        </v-col>
+        </v-row>
+        <v-row justify="center">
+        <v-col cols="auto">
+        </v-col>
+        </v-row>
+        <v-row justify="center">
+        <v-col cols="auto">
+        </v-col>
+        </v-row>        
+        <v-row justify="center">
+        <v-col cols="auto">
+        </v-col>
+        </v-row>
+        <v-row justify="center">
+        <v-col cols="auto">
+        </v-col>
+        </v-row>
+
+        <v-row justify="center">
+
+            <v-col cols="auto">
+            <v-btn  size="x-large" class="mx-2" :to="{ name: 'characters' }">Characters Roles</v-btn>
+            </v-col>
+
+            <v-col cols="auto">
+            <v-btn  size="x-large" class="mx-2" :to="{ name: 'countries' }">Story Countries</v-btn>
+            </v-col>
+        </v-row>
+         <v-row justify="center">
+
+            <v-col cols="auto">
+            <v-btn  size="x-large" class="mx-2" :to="{ name: 'languages' }">Story Languages</v-btn>
+            </v-col>
+
+            <v-col cols="auto">
+            <v-btn  size="x-large" class="mx-2" :to="{ name: 'settings' }">Story Settings</v-btn>
+            </v-col>
+        </v-row>
+    </div>
+  </v-container>
+</template>
