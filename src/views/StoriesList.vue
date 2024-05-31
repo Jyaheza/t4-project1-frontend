224a99737279847b23e6906c1641ea655b002505
Mark <sandmdelgados@gmail.com>
2024-05-30 23:04:58 -0500

    PR Feedback. Applying Vue formatter.

--- src/views/StoriesList.vue
+++ src/views/StoriesList.vue
@@ -162,76 +162,43 @@ function closeSnackBar() {
   <v-container>
     <div id="body">
       <v-row align="center" class="mb-4">
-        <v-col cols="10"
-          ><v-card-title class="pl-0 text-h4 font-weight-bold"
-            >Stories
-          </v-card-title>
+        <v-col cols="10"><v-card-title class="pl-0 text-h4 font-weight-bold">Stories</v-card-title>
         </v-col>
         <v-col class="d-flex justify-end" cols="2">
-          <v-btn v-if="user !== null" color="accent" @click="openAdd()"
-            >Add</v-btn
-          >
+          <v-btn v-if="user !== null" color="accent" @click="openAdd()">Add</v-btn>
         </v-col>
       </v-row>
 
-      <StoryCard
-        v-for="story in stories"
-        :story="story"
-        @deletedList="getLists()"
-      />
+      <StoryCard v-for="story in stories" :story="story" @deletedList="getLists()" />
 
       <v-dialog persistent v-model="isAdd" width="800">
         <v-card class="rounded-lg elevation-5">
           <v-card-title class="headline mb-2">Add Recipe </v-card-title>
           <v-card-text>
-            <v-text-field
-              v-model="newRecipe.name"
-              label="Name"
-              required
-            ></v-text-field>
+            <v-text-field v-model="newRecipe.name" label="Name" required>
+            </v-text-field>
 
-            <v-text-field
-              v-model.number="newRecipe.servings"
-              label="Number of Servings"
-              type="number"
-            ></v-text-field>
-            <v-text-field
-              v-model.number="newRecipe.time"
-              label="Time to Make (in minutes)"
-              type="number"
-            ></v-text-field>
+            <v-text-field v-model.number="newRecipe.servings" label="Number of Servings" type="number">
+            </v-text-field>
+            <v-text-field v-model.number="newRecipe.time" label="Time to Make (in minutes)" type="number">
+            </v-text-field>
 
-            <v-textarea
-              v-model="newRecipe.description"
-              label="Description"
-            ></v-textarea>
-            <v-switch
-              v-model="newRecipe.isPublished"
-              hide-details
-              inset
-              :label="`Publish? ${newRecipe.isPublished ? 'Yes' : 'No'}`"
-            ></v-switch>
+            <v-textarea v-model="newRecipe.description" label="Description">
+            </v-textarea>
+            <v-switch v-model="newRecipe.isPublished" hide-details inset
+              :label="`Publish? ${newRecipe.isPublished ? 'Yes' : 'No'}`"></v-switch>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn variant="flat" color="secondary" @click="closeAdd()"
-              >Close</v-btn
-            >
-            <v-btn variant="flat" color="primary" @click="addRecipe()"
-              >Add Recipe</v-btn
-            >
+            <v-btn variant="flat" color="secondary" @click="closeAdd()">Close</v-btn>
+            <v-btn variant="flat" color="primary" @click="addRecipe()">Add Recipe</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
       <v-snackbar v-model="snackbar.value" rounded="pill">
         {{ snackbar.text }}
-
         <template v-slot:actions>
-          <v-btn
-            :color="snackbar.color"
-            variant="text"
-            @click="closeSnackBar()"
-          >
+          <v-btn :color="snackbar.color" variant="text" @click="closeSnackBar()">
             Close
           </v-btn>
         </template>
