6cc71a7843419100d6f6c943dd2cf41a6b8068a6
carl jackson <c.jackson@eagles.oc.edu>
2024-06-01 11:04:09 -0500

    adding the maintain services pages

---
+++ src/services/CharacterServices.js
@@ -0,0 +1,19 @@
+import apiClient from "./services";
+
+export default {
+  getCharacters() {
+    return apiClient.get("characters");
+  },
+  getCharacters(id) {
+    return apiClient.get("characters/" + id);
+  },
+  addCharacters(character) {
+    return apiClient.post("characters", character);
+  },
+  updateCharacters(character) {
+    return apiClient.put("characters/" + character.id, character);
+  },
+  deleteCharacters(characterId) {
+    return apiClient.delete("characters/" + characterId);
+  },
+};
---
+++ src/services/SettingsServices.js
@@ -0,0 +1,19 @@
+import apiClient from "./services";
+
+export default {
+  getSettings() {
+    return apiClient.get("settings");
+  },
+  getSettings(id) {
+    return apiClient.get("settings/" + id);
+  },
+  addSettings(setting) {
+    return apiClient.post("settings", setting);
+  },
+  updateSettings(setting) {
+    return apiClient.put("settings/" + setting.id, setting);
+  },
+  deleteSettings(settingId) {
+    return apiClient.delete("settings/" + settingId);
+  },
+};
