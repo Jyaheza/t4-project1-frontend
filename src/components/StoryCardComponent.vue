30af3d753c3cef457279ddffc8e72254db5b76fe
Mark <sandmdelgados@gmail.com>
2024-06-01 21:33:48 -0500

    - Added read stories route. - Updated story card component for layout fixes, adding story title and pdf icon. - Updating the getStory method in the Story Services to hit a unique endpoint from Get stories by user id. - Added a ReadStory view. - Updated th e Stories List view to respond to emission from story card component.

--- src/components/StoryCardComponent.vue
+++ src/components/StoryCardComponent.vue
@@ -1,43 +1,54 @@
 <script setup>
-import { ref } from "vue";
+import { ref, defineProps, defineEmits } from 'vue';
 
 const showStory = ref(false);
 
-defineProps({
+const props = defineProps({
     story: {
         required: true,
     },
 });
 
-function deleteStory(){
+const emit = defineEmits(['storySelected']);
+
+function deleteStory() {
     alert('Delete story not implemented yet.')
 }
 
-function editStory(){
+function editStory() {
     alert('Edit story not implemented yet.');
+}
+
+function generatePDF(){
+    alert('PDF generation not implemented yet.');   
+}
+
+function navigateToStoryDetail() {
+    emit('storySelected', props.story.storyId);
 }
 
 </script>
 
 <template>
-    <v-card class="rounded-lg elevation-5 mb-8" @click="showStory = !showStory">
+    <v-card class="rounded-lg elevation-5 mb-8" @click="navigateToStoryDetail">
+        <v-card-title class="headline">
+            {{ `${story.title}` }}
+        </v-card-title>
         <v-card-text class="body-1">
             {{ `${story.story.substring(0, 100)}... ` }}
         </v-card-text>
         <v-card-title class="headline">
             <v-row align="center">
-                <v-col class="d-flex justify-end">
-                    <v-icon
-                        size="small"
-                        icon="mdi-delete"
-                        @click.stop="deleteStory();">
+                <v-col cols="5" class="d-flex justify-end">
+                    <v-icon size="small" icon="mdi-delete" @click.stop="deleteStory();">
                     </v-icon>
                 </v-col>
-                <v-col class="justify-end">
-                    <v-icon
-                        size="small"
-                        icon="mdi-pencil"
-                        @click.stop="editStory();">
+                <v-col cols="1" class="d-flex justify-end">
+                    <v-icon size="small" icon="mdi-pencil" @click.stop="editStory();">
+                    </v-icon>
+                </v-col>
+                <v-col cols="1" class="d-flex justify-end">
+                    <v-icon size="small" icon="mdi-file-pdf-box" @click.stop="generatePDF();">
                     </v-icon>
                 </v-col>
             </v-row>
