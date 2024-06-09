f896a7cbdeb6299aaaaec977d2c82de819d35222
Mark <sandmdelgados@gmail.com>
2024-06-08 22:55:30 -0500

    trying to get rid of "recipe" nomanclature so app runs on live server.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vuetify from "vite-plugin-vuetify";
 
 export default () => {
   const baseURL =
-    process.env.APP_ENV === "development" ? "/" : "/recipe-frontend/";
+    process.env.APP_ENV === "development" ? "/" : "/storytime-frontend/";
 
   return defineConfig({
     plugins: [vue(), vuetify({ autoImport: true })],
